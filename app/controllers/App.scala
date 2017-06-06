4718c5ce18e29aa491d9478373651b8bf70e9590
Susie Coleman <susie.coleman@guardian.co.uk>
2017-06-06 11:40:41 +0100

    WIP experimenting with DB queries.

--- app/controllers/App.scala
+++ app/controllers/App.scala
@@ -3,6 +3,7 @@ package controllers
 import cats.syntax.either._
 import config.Config
 import database.MetricsDB
+import org.joda.time.DateTime
 import play.api.Logger
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -16,6 +17,11 @@ class App(val wsClient: WSClient, val config: Config, val db: MetricsDB) extends
     Ok(views.html.index())
   }
 
+  def startedInComposer(desk: Option[String], startDate: Option[String], endDate: Option[String]) = Action {
+    println(convertStringToDate(startDate))
+    Ok
+  }
+
   def createdInCopyContent = AuthAction { req =>
     val x = for {
       body <- extractRequestBody(req.body.asJson.map(_.toString))
@@ -23,5 +29,12 @@ class App(val wsClient: WSClient, val config: Config, val db: MetricsDB) extends
       data <- jsonToInCopyData(json)
     } yield data
     Ok
+  }
+
+  private def convertStringToDate(date: Option[String]): Option[DateTime] = {
+    date match {
+      case Some(d) => Some(new DateTime(d))
+      case _ => None
+    }
   }
 }
