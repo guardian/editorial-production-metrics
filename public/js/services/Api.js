161b821fd8c19c227c0450c1087528f61d2ad8ab
Antonio Voce <antonio.voce@guardian.co.uk>
2017-08-07 16:24:53 +0100

    Add axios and originatingSystem calls

---
+++ public/js/services/Api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import moment from 'moment';
+
+export const httpClient = axios.create({
+    baseURL: 'https://productionmetrics.local.dev-gutools.co.uk/',
+    timeout: 5000,
+    headers: {
+    }
+});
+
+const getOriginatingSystem = (system, startDate, endDate, desk) =>
+    httpClient.get(`api/originatingSystem/${system}`, {
+        params: {
+            startDate: moment(startDate).format(),
+            endDate: moment(endDate).format(),
+            desk
+        }
+    });
+
+const getComposerVsIncopy = (startDate, endDate, desk) =>
+    axios.all([
+        getOriginatingSystem('composer', startDate, endDate, desk),
+        getOriginatingSystem('incopy', startDate, endDate, desk)
+    ]).then(
+        axios.spread((composerResponse, inCopyResponse) => [{ data: composerResponse.data }, { data: inCopyResponse.data }])
+    );
+
+export default {
+    getOriginatingSystem,
+    getComposerVsIncopy
+};
