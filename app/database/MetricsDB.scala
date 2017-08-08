1582fb025238df39e8c8eb371caf456ec3e154bf
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-08-08 13:08:17 +0100

    Add comment and rename function

--- app/database/MetricsDB.scala
+++ app/database/MetricsDB.scala
@@ -2,7 +2,6 @@ package database
 
 import java.sql.Timestamp
 
-import com.github.tototoshi.slick.PostgresJodaSupport._
 import models.db._
 import slick.jdbc.PostgresProfile.api._
 import models.db.Schema._
@@ -23,10 +22,13 @@ class MetricsDB(val db: Database) {
   def getForks: Seq[Fork] = await(db.run(forksTable.result))
   def insertFork(fork: Fork): Int = await(db.run(forksTable += fork))
 
-  val dayOfWeek = SimpleFunction.binary[String, Timestamp, Timestamp]("date_trunc")
+  val dateTrunc: (Rep[String], Rep[Timestamp]) => Rep[Timestamp] =
+    SimpleFunction.binary[String, Timestamp, Timestamp]("date_trunc")
 
+  // This needs to return the data grouped by day. For this we've defined dateTrunc to tell Slick
+  // to "import" the date_trunc function from postgresql
   def getStartedInSystem(implicit filters: MetricsFilters): Seq[(DateTime, Int)] =
-    await(db.run(metricsTable.filter(MetricsFilters.metricFilters).map(m => (m.id, dayOfWeek("day", m.creationTime))).groupBy(_._2).map{
+    await(db.run(metricsTable.filter(MetricsFilters.metricFilters).map(m => (m.id, dateTrunc("day", m.creationTime))).groupBy(_._2).map{
       case (date, metric) => (date, metric.size)
     }.result)).map(pair => (new DateTime(pair._1), pair._2))
 }
