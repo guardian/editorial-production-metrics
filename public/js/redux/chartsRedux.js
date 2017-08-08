1940ad7cce8bb72a7cb104cc61028d7628671649
Antonio Voce <antonio.voce@guardian.co.uk>
2017-08-08 16:48:29 +0100

    Add basic error management

--- public/js/redux/chartsRedux.js
+++ public/js/redux/chartsRedux.js
@@ -1,29 +1,55 @@
 import { State, Actions, Effect } from 'jumpstate';
-import api from 'services/api';
+import api from 'services/Api';
 import { createYTotalsList, createPartialsList, formattedSeries } from 'helpers/chartsHelpers';
 
 /* ------------- State Management ------------- */
 
 Effect('filterDesk', (filterObj) => {
+    Actions.resetErrors();
     Actions.updateFilter(filterObj);
     Actions.toggleIsUpdatingCharts(true);
     api.getComposerVsIncopy(filterObj.startDate, filterObj.endDate, filterObj.desk)
         .then(composerVsInCopyData => {
             Actions.toggleIsUpdatingCharts(false);
             Actions.updateComposerVsIncopy(composerVsInCopyData);
+        })
+        .catch((error) => {
+            Actions.toggleIsUpdatingCharts(false);
+            Actions.getComposerVsIncopyFailed(error);
         });
 });
 
 const chartsRedux = State({
     initial: {
-        composerVsInCopy: []
+        composerVsInCopy: [],
+        errors: {
+            hasErrors: false,
+            message: null
+        }
     },
 
-    updateComposerVsIncopy(state, chartsData) {
-        const series = chartsData.composerVsInCopy;
-        const totals = createYTotalsList(createPartialsList(series));
-        const percentSeries = formattedSeries(series, totals);
-        return { composerVsInCopy: percentSeries };
+    updateComposerVsIncopy(state, composerVsInCopyData) {
+        const totals = createYTotalsList(createPartialsList(composerVsInCopyData));
+        const percentSeries = formattedSeries(composerVsInCopyData, totals);
+        return Object.assign({}, state, { composerVsInCopy: percentSeries });
+    },
+
+    getComposerVsIncopyFailed(state, error) {
+        return Object.assign({}, state, {
+            errors: { 
+                hasErrors: true,
+                message: error
+            }
+        });
+    },
+
+    resetErrors(state) {
+        return Object.assign({}, state, {
+            errors: {
+                hasErrors: false,
+                message: null
+            }
+        });
     }
 });
 
