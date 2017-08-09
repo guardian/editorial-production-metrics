d72214bedd5788e388c9bf5f3bb61628f119481d
Antonio Voce <antonio.voce@guardian.co.uk>
2017-08-09 14:30:08 +0100

    Revert date formatting changes

--- public/js/helpers/chartsHelpers.js
+++ public/js/helpers/chartsHelpers.js
@@ -1,5 +1,3 @@
-import moment from 'moment';
-
 // Creates a list of all y values from the x,y value pairs in a series' dataset
 const createPartialsList = (series) => {
     let partialsList = new Array(series.length);
@@ -22,16 +20,14 @@ const createYTotalsList = (partialsList) => {
 
 const convertToPercentage = (value, total) => value * 100 / total;
 
-const formatDate = date => moment(date).valueOf();
-
-// Converts the y value in a data pair to its percentage value (using the y totals value) and the x value to its milliseconds date value
-const formatDatasetPair = (pair, total) => { 
-    return {'x': formatDate(pair['x']), 'y': convertToPercentage(pair['y'], total)};
+// Converts the y value in a data pair to its percentage value (using the y totals value)
+const percentageDataSetPair = (pair, total) => { 
+    return {'x': pair['x'], 'y': convertToPercentage(pair['y'], total)};
 };
 
 // Converts the whole dataset y values to percentage, using the y totals collection
 const percentageDataSet = (dataSet, yTotalsList) => {
-    return dataSet.map((pair, index) => formatDatasetPair(pair, yTotalsList[index]));
+    return dataSet.map((pair, index) => percentageDataSetPair(pair, yTotalsList[index]));
 };
 
 // Converts all datasets y values in a series to their percentage value
