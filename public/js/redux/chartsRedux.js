7e669d4c3ffebb94a4f857007f5f2ac571bf32d5
Antonio Voce <antonio.voce@guardian.co.uk>
2017-08-14 16:47:31 +0100

    Integrate API to return 0 data on empty response

--- public/js/redux/chartsRedux.js
+++ public/js/redux/chartsRedux.js
@@ -1,18 +1,20 @@
 import { State, Actions, Effect } from 'jumpstate';
+import moment from 'moment';
 import api from 'services/Api';
 import chartList from 'utils/chartList';
-import { createYTotalsList, createPartialsList, formattedSeries } from 'helpers/chartsHelpers';
+import { createYTotalsList, createPartialsList, formattedSeries, compareDates, fillMissingDates } from 'helpers/chartsHelpers';
 
 /* ------------- State Management ------------- */
 
 Effect('filterDesk', (filterObj) => {
     Actions.updateFilter(filterObj);
     Actions.toggleIsUpdatingCharts(true);
+    const { startDate, endDate, desk } = filterObj;
     chartList.map(chart => {
-        api[`get${chart}`](filterObj.startDate, filterObj.endDate, filterObj.desk)
+        api[`get${chart}`](startDate, endDate, desk)
             .then(chartData => {
                 Actions.toggleIsUpdatingCharts(false);
-                Actions[`update${chart}`](chartData);
+                Actions[`update${chart}`]({ chartData, startDate, endDate });
             })
             .catch(error => {
                 Actions.toggleIsUpdatingCharts(false);
@@ -28,9 +30,15 @@ const chartsRedux = State({
         }
     },
 
-    updateComposerVsIncopy(state, composerVsInCopyData) {
+    updateComposerVsIncopy(state, { chartData, startDate, endDate }) {
+        const { composerResponse, inCopyResponse } = chartData;
+        const range = endDate.diff(startDate, 'days');
+        const composerData = composerResponse.data.length < range ?  fillMissingDates(startDate, endDate, composerResponse.data).sort(compareDates) : composerResponse.data;
+        const inCopyData = inCopyResponse.data.length < range ?  fillMissingDates(startDate, endDate, inCopyResponse.data).sort(compareDates) : inCopyResponse.data;
+        const composerVsInCopyData = [{ data: composerData }, { data: inCopyData }];
         const totals = createYTotalsList(createPartialsList(composerVsInCopyData));
         const percentSeries = formattedSeries(composerVsInCopyData, totals);
+
         return { 
             composerVsInCopy: {
                 data: percentSeries
