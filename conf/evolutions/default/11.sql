c3af610edc365737b05098fe8b32a412d0b5a497
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-09-21 12:09:55 +0100

    Rename field

--- conf/evolutions/default/11.sql
+++ conf/evolutions/default/11.sql
@@ -1,9 +1,9 @@
 # --- !Ups
 
 ALTER TABLE forks ADD COLUMN issue_date timestamp;
-ALTER TABLE forks ADD COLUMN time_until_fork integer;
+ALTER TABLE forks ADD COLUMN seconds_until_fork integer;
 
 # --- !Downs
 
 ALTER TABLE forks DROP COLUMN issue_date;
-ALTER TABLE forks DROP COLUMN time_until_fork;
+ALTER TABLE forks DROP COLUMN seconds_until_fork;
