4ed282e4641fa127a085be542fdd899d8fa7c3a2
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-09-28 10:39:34 +0100

    Add updateMetric unit test

--- test/BaseSuite.scala
+++ test/BaseSuite.scala
@@ -1,3 +1,8 @@
+import java.util.TimeZone
+
 import org.scalatest.{FunSuite, Matchers}
 
-class BaseSuite extends FunSuite with Matchers
+class BaseSuite extends FunSuite with Matchers {
+  System.setProperty("user.timezone", "UTC")
+  TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+}
--- test/MetricSpec.scala
+++ test/MetricSpec.scala
@@ -8,12 +8,21 @@ class MetricSpec extends BaseSuite {
       id = "id123",
       originatingSystem = OriginatingSystem.Composer,
       creationTime = new DateTime("2017-08-01T00:00:00Z"),
-      firstPublicationTime = Some(new DateTime("2017-08-31T00:00:00Z")),
       inWorkflow = true
     )
     val metricOpt = MetricOpt(
-      originatingSystem = Some(OriginatingSystem.InCopy),
-      inWorkflow = Some(true)
+      originatingSystem = Some(OriginatingSystem.InCopy), //override something from Metric
+      inWorkflow = Some(true), //same value as in Metric
+      newspaperBook = Some("guardian/test"), //new value
+      id = None //this should not replace the actual value
     )
+    val expectedResult = Metric(
+      id = metric.id,
+      originatingSystem = metricOpt.originatingSystem.get,
+      creationTime = metric.creationTime,
+      inWorkflow = metricOpt.inWorkflow.get,
+      newspaperBook = metricOpt.newspaperBook
+    )
+    Metric.updateMetric(metric, metricOpt) should equal(Right(expectedResult))
   }
 }
