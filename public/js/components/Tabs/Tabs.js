88ca44323429db87a3597bbf4fbc3060f2abd260
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-03 18:22:31 +0000

    Nicer spacing for tabs.

--- public/js/components/Tabs/Tabs.js
+++ public/js/components/Tabs/Tabs.js
@@ -1,5 +1,5 @@
 import React, { Component, Children } from 'react';
-import { Grid } from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 export default class Tabs extends Component {
     state = {selectedIndex: 0};
@@ -13,7 +13,13 @@ export default class Tabs extends Component {
         const selectedTab = Children.toArray(this.props.children)[this.state.selectedIndex];
         return (
             <Grid fluid>
-                {this.props.labels.map((label, index) => <button key={label} onClick={event => this.onChange(event, index)}>{label}</button>)}
+                <Row between="xs">
+                {this.props.labels.map((label, index) => 
+                    <Col xs key={label}>
+                        <button key={label} onClick={event => this.onChange(event, index)}>{label}</button>
+                    </Col>
+                )}
+                </Row>
                 {selectedTab}
             </Grid>
         );
