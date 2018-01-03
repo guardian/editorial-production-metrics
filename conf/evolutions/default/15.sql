c5dc9d63b79b45e54178d71fd80adf71df0bb180
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-01-03 11:38:22 +0000

    Add new columns to metrics table to display commissioned length

---
+++ conf/evolutions/default/15.sql
@@ -0,0 +1,11 @@
+# --- !Ups
+
+ALTER TABLE metrics ADD COLUMN word_count integer;
+ALTER TABLE metrics ADD COLUMN commissioned_word_count integer;
+ALTER TABLE metrics ADD COLUMN path varchar;
+
+# --- !Downs
+
+ALTER TABLE metrics DROP COLUMN word_count;
+ALTER TABLE metrics DROP COLUMN commissioned_word_count;
+ALTER TABLE metrics ADD COLUMN path;
