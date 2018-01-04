06adcfc7c8f414248cd21db1ec77c8d6d1a397c7
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-04 12:43:32 +0000

    Use newspaper book with more data.

--- public/js/reducers/updateFilterReducer.js
+++ public/js/reducers/updateFilterReducer.js
@@ -4,7 +4,7 @@ const getYesterday = () => { return moment().utc().startOf('day').subtract(1, 'd
 
 const initialState = {
     desk: 'tracking/commissioningdesk/all',
-    newspaperBook: 'theguardian/main', // TODO: set this programatically
+    newspaperBook: 'theguardian/mainsection', // TODO: set this programatically
     productionOffice: 'all',
     startDate: getYesterday().startOf('day').subtract(7,'d').format(),
     endDate: getYesterday().endOf('day').format()
