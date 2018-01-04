8a4e07d70440a50941cf05f535f8dbb02ad84325
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-04 16:29:26 +0000

    Add basic table for articles.

--- public/js/components/Tabs/_commissionedLengthTestData.js
+++ public/js/components/Tabs/_commissionedLengthTestData.js
@@ -18,10 +18,20 @@ const articleCount = sumCounts(wordCountBands);
 const withCommissionedLengthCount = sumCounts(commissionedLengthBands);
 const withoutCommissionedLengthCount = articleCount - withCommissionedLengthCount;
 
+const articles = [
+  {headline: "Meltdown and Spectre: ‘worst ever’ CPU bugs affect virtually all computers", path:"technology/2018/jan/04/meltdown-spectre-worst-cpu-bugs-ever-found-affect-computers-intel-processors-security-flaw", wordCount: 650, commissionedWordCount: 600},
+  {headline: "Are there any good portable MP3 players for blind and visually impaired people?", path:"technology/askjack/2018/jan/04/apple-ipod-shuffle-good-mp3-portable-music-players-blind-visually-impaired-users", wordCount: 750, commissionedWordCount: 800},
+  {headline: "Facebook declines to say why it deletes certain political accounts, but not others", path:"us-news/2018/jan/04/facebook-chechnya-ramzan-kadyrov-political-censorship", wordCount: 928, commissionedWordCount: 600},
+  {headline: "Shares in spread betting firm Plus500 soar thanks to bitcoin boom ", path:"technology/2018/jan/03/spread-betting-firm-plus500s-shares-soar-bitcoin-boom", wordCount: 834, commissionedWordCount: 900},
+  {headline: "Apple leads race to become world's first $1tn company", path:"business/2018/jan/03/apple-leads-race-to-become-world-first-1tn-dollar-company", wordCount: 340, commissionedWordCount: 350},
+  {headline: "Neurotechnology, Elon Musk and the goal of human enhancement ", path:"technology/2018/jan/01/elon-musk-neurotechnology-human-enhancement-brain-computer-interfaces", wordCount: 1000, commissionedWordCount: 0},
+]
+
 export default {
   wordCountBands,
   commissionedLengthBands,
   articleCount, // number
   withCommissionedLengthCount, // number
   withoutCommissionedLengthCount, // number
+  articles
 };
