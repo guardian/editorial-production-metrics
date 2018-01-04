2a76cf5d7330790c031fd4eca7cb229d4ce375ea
RichieAHB <richard.beddington@gmail.com>
2018-01-04 14:40:08 +0000

    [WIP] added some commissioning length placeholder stuff

--- public/js/components/Tabs/CommissionedLength.js
+++ public/js/components/Tabs/CommissionedLength.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import BandedCount from "../BandedCount";
+import PieChartWrap from "../Charts/PieChartWrap";
+import TEST_DATA from "./_commissionedLengthTestData";
+import { bandName } from "../../utils/BandUtils";
 
 const CommissionedLength = () => {
     return (
         <div>
-            I'm the Commissioned Length tab
+            <PieChartWrap data ={TEST_DATA.wordCountBands.map(({ count, ...band }) => ({
+                x: bandName(band),
+                y: count
+            }))} />
+            <BandedCount data={TEST_DATA.wordCountBands} />
+            <BandedCount data={TEST_DATA.commissionedLengthBands} />
         </div>
     );
 };
