6aa47d722838f3b23258dac6bfa25c0133680f7d
RichieAHB <richard.beddington@gmail.com>
2018-01-04 16:51:20 +0000

    Hijack some table styles and add Table components

---
+++ public/js/components/Table/Table.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Th } from './index';
+
+const tableClass = centered =>
+  [
+    `table__table`,
+    centered ? `table__table--centered` : ""
+  ]
+    .join(" ")
+    .replace(/\s+/, " ")
+    .trim();
+
+const renderSection = (leftTitle, children, propFn) =>
+  React.Children.map(children, (child, index) =>
+    React.cloneElement(child, {
+      ...propFn(child, index),
+      children:
+        index === 0 && leftTitle
+          ? [
+              <Th key="header" rowSpan={React.Children.count(children)}>
+                {leftTitle}
+              </Th>
+            ].concat(React.Children.toArray(child.props.children))
+          : child.props.children
+    })
+  );
+
+const Table = ({ head, children, centered, headTitle, bodyTitle, alternate }) => (
+  <table className={tableClass(centered)}>
+    {head && (
+      <thead className="table__head">
+        {renderSection(headTitle, head, () => ({ headerRow: true }))}
+      </thead>
+    )}
+    <tbody className="table__body">
+      {renderSection(bodyTitle, children, (_, i) => ({ isOdd: alternate ? !(i % 2) : null }))}
+    </tbody>
+  </table>
+);
+
+export default Table;
