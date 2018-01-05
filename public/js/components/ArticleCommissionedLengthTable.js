e7e7beffea42805542ef0803e2f68c78b2f87500
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-05 11:02:08 +0000

    Better javascript style.

--- public/js/components/ArticleCommissionedLengthTable.js
+++ public/js/components/ArticleCommissionedLengthTable.js
@@ -4,7 +4,7 @@ import {Table, Tr, Th, Td} from './Table';
 const compare = (article1, article2) => article2.commissionedWordCount - article1.commissionedWordCount;
 
 const ArticleCommissionedLengthTable = ({articles}) => {
-    const noCommissioningLength = articles.filter(article => article.commissionedWordCount == 0)
+    const noCommissioningLength = articles.filter(article => article.commissionedWordCount === 0)
     const sortedArticles = articles.filter(article => article.commissionedWordCount > 0).sort(compare)
     return(
         <div>
@@ -18,8 +18,8 @@ const ArticleCommissionedLengthTable = ({articles}) => {
             }>
                 {noCommissioningLength.map(x => 
                     <Tr key={x.path}>
-                        <Td><a href={"https://www.theguardian.com/"+x.path} target="_blank" rel="noopener noreferrer">{x.headline}</a></Td>
-                        <Td><a href={"https://dashboard.ophan.co.uk/info?path=/"+x.path} target="_blank" rel="noopener noreferrer">Ophan</a></Td>
+                        <Td><a href={`https://www.theguardian.com/${x.path}`} target="_blank" rel="noopener noreferrer">{x.headline}</a></Td>
+                        <Td><a href={`https://dashboard.ophan.co.uk/info?path=/${x.path}`} target="_blank" rel="noopener noreferrer">Ophan</a></Td>
                         <Td>{x.commissionedWordCount}</Td>
                     </Tr>
                 )}
@@ -37,8 +37,8 @@ const ArticleCommissionedLengthTable = ({articles}) => {
             }>
                 {sortedArticles.map(x => 
                     <Tr key={x.path}>
-                        <Td><a href={"https://www.theguardian.com/"+x.path} target="_blank" rel="noopener noreferrer">{x.headline}</a></Td>
-                        <Td><a href={"https://dashboard.ophan.co.uk/info?path=/"+x.path} target="_blank" rel="noopener noreferrer">Ophan</a></Td>
+                        <Td><a href={`https://www.theguardian.com/${x.path}`} target="_blank" rel="noopener noreferrer">{x.headline}</a></Td>
+                        <Td><a href={`https://dashboard.ophan.co.uk/info?path=/${x.path}`} target="_blank" rel="noopener noreferrer">Ophan</a></Td>
                         <Td>{x.wordCount}</Td>
                         <Td>{x.commissionedWordCount}</Td>
                         <Td>{x.wordCount - x.commissionedWordCount}</Td>
