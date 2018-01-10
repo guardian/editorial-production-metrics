2fe96276fff21f1952ef79a666183d9b880b8e8b
RichieAHB <richard.beddington@gmail.com>
2018-01-10 12:16:42 +0000

    Add memoized selectors for chart data

--- public/js/helpers/chartsHelpers.js
+++ public/js/helpers/chartsHelpers.js
@@ -98,4 +98,60 @@ const downloadCSV = (data, chartType, filterVals) => {
     }
 };
 
-export { createPartialsList, formattedSeries, createYTotalsList, compareDates, compareIssueDates, fillMissingDates, downloadCSV, tagToName };
+const tidyData = (data, startDate, endDate) => {
+    const range = endDate.diff(startDate, "days");
+    return data.length <= range
+        ? fillMissingDates(startDate, endDate, data).sort(compareDates)
+        : data.sort(compareDates);
+};
+
+const getComparisonTimeSeriesFromResponses = (
+    res1,
+    res2,
+    startDate,
+    endDate,
+    countLabel1,
+    countLabel2
+) => {
+    const data1 = tidyData(res1.data, startDate, endDate);
+    const data2 = tidyData(res2.data, startDate, endDate);
+    const series = [{ data: data1 }, { data: data2 }];
+
+    const absolute = series.map(({ data }) => {
+        return {
+            data: data.map((dataPoint, i) => {
+                const date = moment(dataPoint["date"]).utc();
+                const formattedDate = date.format("ddd, Do MMMM YYYY");
+                return {
+                    x: date.valueOf(),
+                    y: dataPoint["count"],
+                    label: `
+                        Date: ${formattedDate}\n
+                        ${countLabel1}: ${series[0].data[i].count}\n
+                        ${countLabel2}: ${series[1].data[i].count}
+                    `
+                };
+            })
+        };
+    });
+
+    const totals = createYTotalsList(createPartialsList(absolute));
+    const percent = formattedSeries(absolute, totals);
+
+    return {
+        absolute,
+        percent
+    };
+};
+
+export {
+    createPartialsList,
+    formattedSeries,
+    createYTotalsList,
+    compareDates,
+    compareIssueDates,
+    fillMissingDates,
+    downloadCSV,
+    tagToName,
+    getComparisonTimeSeriesFromResponses
+};
--- public/js/selectors/charts.js
+++ public/js/selectors/charts.js
@@ -1,126 +1,106 @@
 import moment from "moment";
 import {
-    createYTotalsList,
-    createPartialsList,
-    formattedSeries,
-    compareDates,
     compareIssueDates,
-    fillMissingDates
+    getComparisonTimeSeriesFromResponses
 } from "../helpers/chartsHelpers";
+import { createSelector } from "reselect";
 
 const getCharts = ({ charts }) => charts;
+
 const getMoments = ({ startDate, endDate }) => ({
     startDate: moment(startDate),
     endDate: moment(endDate)
 });
 
-const createComposerVsIncopyData = chart => {
-    const {
-        chartData: { composerResponse, inCopyResponse },
-        pending
-    } = chart;
+/* Composer vs Incopy */
 
-    if (pending) {
-        return {
-            absolute: [],
-            percent: []
-        };
+const getRawComposerVsIncopy = createSelector(
+    getCharts,
+    ({ composerVsInCopy }) => composerVsInCopy
+);
+
+const getComposerVsIncopyData = createSelector(
+    getRawComposerVsIncopy,
+    composerVsInCopy => {
+        const {
+            chartData: { composerResponse, inCopyResponse },
+            pending
+        } = composerVsInCopy;
+
+        if (pending) {
+            return {
+                absolute: [],
+                percent: []
+            };
+        }
+
+        const { startDate, endDate } = getMoments(composerVsInCopy);
+
+        return getComparisonTimeSeriesFromResponses(
+            composerResponse,
+            inCopyResponse,
+            startDate,
+            endDate,
+            "Created in composer",
+            "Created in InCopy"
+        );
     }
+);
 
-    const { startDate, endDate } = getMoments(chart);
-
-    const range = endDate.diff(startDate, 'days');
-    const composerData = composerResponse.data.length <= range ?  fillMissingDates(startDate, endDate, composerResponse.data).sort(compareDates) : composerResponse.data.sort(compareDates);
-    const inCopyData = inCopyResponse.data.length <= range ?  fillMissingDates(startDate, endDate, inCopyResponse.data).sort(compareDates) : inCopyResponse.data.sort(compareDates);
-    const composerVsInCopyData = [{ data: composerData }, { data: inCopyData }];
-
-    const seriesWithLabels = composerVsInCopyData.map(series => {
-        return {
-            data: series.data.map((dataPoint, index) => {
-                const date = moment(dataPoint['date']).utc();
-                return {
-                    x: date.valueOf(),
-                    y: dataPoint['count'],
-                    label: `Date: ${date.format('ddd, Do MMMM YYYY')}\nCreated in Composer: ${composerVsInCopyData[0]['data'][index]['count']}\nCreated in InCopy: ${composerVsInCopyData[1]['data'][index]['count']}`
-                };
-            })
-        };
-    });
-    const totals = createYTotalsList(createPartialsList(seriesWithLabels));
-    const percentSeries = formattedSeries(seriesWithLabels, totals);
-
-    return {
-        absolute: seriesWithLabels,
-        percent: percentSeries
-    };
-};
-
-// memoize me
-export const getComposerVsIncopy = state => {
-    const { composerVsInCopy } = getCharts(state);
-    const {
+export const getComposerVsIncopy = createSelector(
+    getRawComposerVsIncopy,
+    getComposerVsIncopyData,
+    ({ error, isStacked }, data) => ({
+        data,
         error,
         isStacked
-    } = composerVsInCopy;
+    })
+);
 
-    return {
-        data: createComposerVsIncopyData(composerVsInCopy),
-        error,
-        isStacked
-    };
-};
+/* In Workflow vs not in Workflow */
 
-const createInWorkflowVsNotInWorkflowData = chart => {
-    const {
-        chartData: { inWorkflowResponse, notInWorkflowResponse },
-        pending,
-    } = chart;
+const getRawInWorkflowVsNotInWorkflow = createSelector(
+    getCharts,
+    ({ inWorkflowVsNotInWorkflow }) => inWorkflowVsNotInWorkflow
+);
 
-    if (pending) {
-        return {
-            absolute: [],
-            percent: []
-        };
+const getInWorkflowVsNotInWorkflowData = createSelector(
+    getRawInWorkflowVsNotInWorkflow,
+    inWorkflowVsNotInWorkflow => {
+        const {
+            chartData: { inWorkflowResponse, notInWorkflowResponse },
+            pending,
+        } = inWorkflowVsNotInWorkflow;
+    
+        if (pending) {
+            return {
+                absolute: [],
+                percent: []
+            };
+        }
+
+        const { startDate, endDate } = getMoments(inWorkflowVsNotInWorkflow);
+    
+        return getComparisonTimeSeriesFromResponses(
+            inWorkflowResponse,
+            notInWorkflowResponse,
+            startDate,
+            endDate,
+            "In Workflow",
+            "Never in Workflow"
+        );
     }
+);
 
-    const { startDate, endDate } = getMoments(chart);
-
-    const range = endDate.diff(startDate, 'days');
-    const inWorkflowData = inWorkflowResponse.data.length <= range ?  fillMissingDates(startDate, endDate, inWorkflowResponse.data).sort(compareDates) : inWorkflowResponse.data.sort(compareDates);
-    const notInWorkflowData = notInWorkflowResponse.data.length <= range ?  fillMissingDates(startDate, endDate, notInWorkflowResponse.data).sort(compareDates) : notInWorkflowResponse.data.sort(compareDates);
-    const workflowVsNotInWorkflowData = [{ data: inWorkflowData }, { data: notInWorkflowData }];
-
-    const seriesWithLabels = workflowVsNotInWorkflowData.map(series => {
-        return {
-            data: series.data.map((dataPoint, index) => {
-                const date = moment(dataPoint['date']).utc();
-                return {
-                    x: date.valueOf(),
-                    y: dataPoint['count'],
-                    label: `Date: ${date.format('ddd, Do MMMM YYYY')}\nIn Workflow: ${workflowVsNotInWorkflowData[0]['data'][index]['count']}\nNever in Workflow: ${workflowVsNotInWorkflowData[1]['data'][index]['count']}`
-                };
-            })
-        };
-    });
-    const totals = createYTotalsList(createPartialsList(seriesWithLabels));
-    const percentSeries = formattedSeries(seriesWithLabels, totals);
-
-    return {
-        absolute: seriesWithLabels,
-        percent: percentSeries
-    };
-};
-
-export const getInWorkflowVsNotInWorkflow = state => {
-    const { inWorkflowVsNotInWorkflow } = getCharts(state);
-    const { error, isStacked } = inWorkflowVsNotInWorkflow;
-
-    return {
-        data: createInWorkflowVsNotInWorkflowData(inWorkflowVsNotInWorkflow),
+export const getInWorkflowVsNotInWorkflow = createSelector(
+    getRawInWorkflowVsNotInWorkflow,
+    getInWorkflowVsNotInWorkflowData,
+    ({ error, isStacked }, data) => ({
+        data,
         error,
         isStacked
-    };
-};
+    })
+);
 
 const createForkTimeData = chart => {
     const { chartData, pending } = chart;
@@ -134,7 +114,6 @@ const createForkTimeData = chart => {
 
     const forkTimeData = chartData.data.sort(compareIssueDates);
     const forkTimeSeries = [{ data: forkTimeData }];
-    console.log(forkTimeSeries);
     return {
         absolute: forkTimeSeries.map(series => ({
             data: series.data.map((dataPoint) => {
