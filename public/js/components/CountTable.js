b970d9b13e13f5440853a308ccadccf177eb92db
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-26 14:36:32 +0000

    Display total number of articles.

--- public/js/components/CountTable.js
+++ public/js/components/CountTable.js
@@ -12,14 +12,22 @@ const CountTable = ({ bands, title }) => (
             head={
                 <Tr>
                     {bands.map(({ label }) => <Th key={label}>{label}</Th>)}
+                    <Th>Total</Th>
                 </Tr>
             }
         >
             <Tr>
                 {bands.map(({ label, count }) => <Td key={label}>{count}</Td>)}
+                <Td>{calculateTotal(bands)}</Td>
             </Tr>
         </Table>
     </div>
 );
+
+function calculateTotal(bands) {
+    let total = 0;
+    bands.map(item => total = total + item.count);
+    return total;
+}
 
 export default CountTable;
