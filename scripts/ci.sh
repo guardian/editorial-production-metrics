b83d738fcbec7cec84b0e5c8a3a826acde9bda2b
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-07-24 11:48:54 +0100

    Use better teamcity script interaction

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-echo "##teamcity[npm 'install yarn']"
+echo "##teamcity[compilationStarted compiler='yarn']"
 npm install --global yarn
-echo "##teamcity[yarn 'install']"
 yarn install
-echo "##teamcity[yarn 'build']"
 yarn build
+echo "##teamcity[compilationFinished compiler='yarn']"
 
 echo "##teamcity[compilationStarted compiler='sbt']"
 sbt clean compile testAll riffRaffUpload
